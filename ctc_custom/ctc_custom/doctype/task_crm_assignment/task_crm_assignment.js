78e63c732466fecf5e8158fb8f7802db13eea204
govi9926 <[govindgupta78090@gmail.com]>
2024-02-23 14:29:26 +0530

    first commit

---
+++ ctc_custom/ctc_custom/doctype/task_crm_assignment/task_crm_assignment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Yes and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Task Crm Assignment', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ ctc_custom/ctc_custom/doctype/task_crm_assignment/test_task_crm_assignment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Yes and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTaskCrmAssignment(FrappeTestCase):
+	pass
